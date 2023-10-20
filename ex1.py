03cb577aa5e92f12da2c3e5078f5c1b77a812820
Mat6806 <147594936+Mat6806@users.noreply.github.com>
2023-10-20 10:52:35 +0200

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,16 @@
+nom = "jean"
+prenom = "kevin"
+math = 12
+anglais = 16
+info = 20
+promotion = "2023"
+moy = (anglais + math + info)/3
+print("L'étudiant", nom, prenom, "de la promotion", promotion, "a une moyenne de", moy)
+
+print(type(nom))
+print(type(prenom))
+print(type(math))
+print(type(anglais))
+print(type(info))
+print(type(promotion))
+print(type(moy))
