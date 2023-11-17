8f54d7e9c95890505f5db0f0fe5da8af96b3e67b
Mat6806 <147594936+Mat6806@users.noreply.github.com>
2023-11-17 14:57:57 +0100

    Add files via upload

---
+++ TP3/TP3_ex1.py
@@ -0,0 +1,62 @@
+print("exercice 1 a") #exo 1 a
+N = int(input("Entrez la valeur de N : "))
+somme = 0
+
+for i in range(N + 1):
+    somme += i
+
+
+print("La somme des N premiers entiers naturels est :", somme)
+
+
+
+print("exercice 1 b") #exo 1 b
+
+valeur_utilisateur = int(input("Entrez une valeur (entre 0 et 100) : "))
+
+while valeur_utilisateur != 100:
+    valeur_utilisateur = int(input("Entrez une valeur (entre 0 et 100) : "))
+
+print("La boucle d'attente se termine. Vous avez entré la valeur 100.")
+
+
+print("exercice 1 c") #exo 1 c
+valeurs_inf_10 = 0
+valeurs_10_a_15 = 0
+valeurs_sup_15 = 0
+
+for _ in range(10):
+    valeur = float(input("Entrez une valeur réelle entre 0 et 20 : "))
+
+    while valeur < 0 or valeur > 20:
+        print("La valeur doit être comprise entre 0 et 20.")
+        valeur = float(input("Entrez une valeur réelle entre 0 et 20 : "))
+
+    if valeur < 10:
+        valeurs_inf_10 += 1
+    elif valeur < 15:
+        valeurs_10_a_15 += 1
+    else:
+        valeurs_sup_15 += 1
+
+print("Le nombre de valeurs inférieures strictement à 10 est :", valeurs_inf_10)
+print("Le nombre de valeurs supérieures ou égales à 10 et inférieures strictement à 15 est :", valeurs_10_a_15)
+print("Le nombre de valeurs supérieures ou égales à 15 est :", valeurs_sup_15)
+
+print("exercice 1 d") #exo 1 d
+X = float(input("Entrez un nombre supérieur à 1 (X) : "))
+
+while X <= 1:
+    print("Veuillez entrer un nombre supérieur à 1.")
+    X = float(input("Entrez un nombre supérieur à 1 (X) : "))
+
+somme = 0
+N = 0
+
+while somme <= X:
+    N += 1
+    somme += N
+
+N -= 1
+
+print("Le plus grand nombre N tel que la somme de 0 à N soit inférieure ou égale à", X, "est :", N)
